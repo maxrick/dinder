85558c2d0a2d1e8d19857c4bb5d4d28a3ba8ac2a
maxRick <maxayzen@gmail.com>
2018-01-12 14:54:51 +0000

    openshift compatibel

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 var express = require('express'),
 app = express(),
-port = process.env.PORT || 3000,
+//port = process.env.PORT || 3000,
+
+port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+dbService = process.env.DATABASE_SERVICE_NAME || 'mongodb'
+mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || '127.0.0.1',
+mongoURLLabel = "",
+db_name = 'dinderdb',
 mongoose = require('mongoose'),
 Task = require('./api/models/todoListModel'), //created model loading here
 bodyParser = require('body-parser');
 
+//copied from nodejs guide
+//provide a sensible default for local development
+mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+//take advantage of openshift env vars when available:
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+}
+
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/tododb'); 
-
+//mongoose.connect('mongodb://localhost/tododb'); 
+mongoose.connect(mongodb_connection_string);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -21,7 +36,7 @@ var routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
 
 
-app.listen(port);
+app.listen(port, ip);
 
 
 console.log('todo list RESTful API server started on: ' + port);
