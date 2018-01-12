b6d010a58db24f8b68dfa709494c85b01c6fba5c
maxRick <maxayzen@gmail.com>
2018-01-12 15:47:35 +0000

    mongodb host corrected

--- server.js
+++ server.js
@@ -5,22 +5,17 @@ app = express(),
 port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
 dbService = process.env.DATABASE_SERVICE_NAME || 'mongodb'
-mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || '127.0.0.1',
-mongoURLLabel = "",
+mongoHost =  process.env.MONGODB_SERVICE_HOST || '127.0.0.1',
 db_name = process.env.MONGODB_DATABASE || 'dinderdb',
 mongoose = require('mongoose'),
 Task = require('./api/models/todoListModel'), //created model loading here
 bodyParser = require('body-parser');
 
 
-console.log('port: '+port+'\nip: '+ ip+ '\ndbService'+dbService+'\nmongoUrl: '+mongoURL+'\ndbname: ' +db_name);
+console.log('port: '+port+'\nip: '+ ip+ '\ndbService: '+dbService+'\nmongoHost: '+mongoHost+'\ndbname: ' +db_name);
 //copied from nodejs guide
 //provide a sensible default for local development
-mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
-//take advantage of openshift env vars when available:
-if(process.env.OPENSHIFT_MONGODB_DB_URL){
-  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
-}
+mongodb_connection_string = 'mongodb://'+mongoHost+':27017/' + db_name;
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
